1f3f424ab05cc9e72e3418deb910918d5052edd6
vanilasky00 <pipichun2@gmail.com>
2025-11-26 10:52:30 +0800

    update: add compress logic and fix: the overflow problem

---
+++ 2025DS/hw4/README.md
@@ -0,0 +1,12 @@
+root list: circular doubly linked list
+child list: circular doubly linked list
+
+code style: 
+[To typedef structs or not](https://www.reddit.com/r/C_Programming/comments/kr4i80/to_typedef_structs_or_not/)
+```
+It's an opinion. Follow your coding standard.
+
+The Linux kernel says no.
+
+OpenSSL says yes (Chapter 5)
+```
--- 2025DS/mock_test/pA.c
+++ 2025DS/mock_test/pA.c
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 typedef struct Node {
-    int score;
+    long long score;
     int temp;
     struct Node *next;
 } Node;
@@ -13,14 +13,39 @@ void freeNode(Node* n) {
     if (n) free(n);
 }
 
-Node *compressAndClean(Node *n) {
+Node *compressAndClean(Node *head) {
+    Node dummy;
+    dummy.next = head;
+    Node *prev = &dummy;
+    Node *curr = head;
 
+    while (curr != NULL) {
+        while (curr->next != NULL && curr->temp == curr->next->temp) {
+            Node *duplicated = curr->next;
+            curr->score += duplicated->score;
+            curr->next = duplicated->next;
+            duplicated->next = NULL;
+            freeNode(duplicated);
+        }
+
+        if (curr->score == 0) {
+            prev->next = curr->next;
+            freeNode(curr);
+            curr = prev->next;
+        }
+        else {
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+
+    return dummy.next;
 }
 
 Node *mergeSortedLists(Node *listA, Node *listB) {
     Node dummy;
     Node *tail = &dummy;
-    dummy.next == NULL;
+    dummy.next = NULL;
 
     while (listA && listB) {
         if (listA->temp > listB->temp) {
@@ -65,13 +90,14 @@ int main(void) {
     int m, n;
     if (scanf("%d%d", &m, &n) != 2) return -1;
 
-    int s, t;
+    long long s;
+    int t;
 
     Node dummyA;
     Node *tailA = &dummyA;
     dummyA.next = NULL;
     while (m-- > 0) {
-        if (scanf("%d%d", &s, &t) != 2) return -1;
+        if (scanf("%lld%d", &s, &t) != 2) return -1;
         tailA->next = createNode(s, t);
         tailA = tailA->next;
     }
@@ -80,7 +106,7 @@ int main(void) {
     Node *tailB = &dummyB;
     dummyB.next = NULL;
     while (n-- > 0) {
-        if (scanf("%d%d", &s, &t) != 2) return -1;
+        if (scanf("%lld%d", &s, &t) != 2) return -1;
         tailB->next = createNode(s, t);
         tailB = tailB->next;
     }
@@ -92,7 +118,7 @@ int main(void) {
     }
     else {
         while (res != NULL) {
-            printf("%d %d\n", res->score, res->temp);
+            printf("%lld %d\n", res->score, res->temp);
             res = res->next;
         }
     }
