64bca1c0ecce67699dff554e80a3e812689fd897
vanilasky00 <pipichun2@gmail.com>
2025-12-03 02:57:41 +0800

    init practice and implement leftis tree with 2 version of meld

---
+++ 2025DS/test_practice/leftis_tree.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Node {
+    struct Node *left;
+    struct Node *right;
+    int key;
+    int s;
+} Node;
+
+void swap(Node **x, Node **y) {
+    Node *tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+Node *meld(Node *x, Node *y) {
+    if (!x) return y;
+    if (!y) return x;
+
+    if (x->key > y->key) swap(&x, &y);
+
+    x->right = meld(x->right, y);
+
+    if (!x->left) {
+        x->left = x->right;
+        x->right = NULL;
+    }
+    else {
+        if (x->left->s < x->right->s) swap(&x->left, &x->right);
+        x->s = x->right->s + 1;
+    }
+
+    return x;
+}
+
+Node *meld_v2(Node *x, Node *y) {
+    if (!x) return y;
+    if (!y) return x;
+
+    if (x->key > y->key) swap(&x, &y);
+
+    x->right = meld_v2(x->right, y);
+
+    int ls = x->left ? x->left->s : -1;
+    int rs = x->right ? x->right->s : -1;
+
+    if (ls < rs) swap(&x->left, &x->right);
+
+    x->s = (x->right ? x->right->s : -1) + 1;
+    return x;
+}
+
+void insert(Node **root, int key) {
+    Node *node = (Node*) calloc(1, sizeof(Node));
+    node->key = key;
+    *root = meld(*root, node);
+}
+
+int extractMin(Node **root) {
+    Node *r = *root;
+    int min = r->key;
+    *root = meld(r->left, r->right);
+    free(r);
+    return min;
+}
+
+void print_leftis_tree(Node *root) {
+
+}
+
+int main(void) {
+
+    return 0;
+}
